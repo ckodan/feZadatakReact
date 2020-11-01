d5319e91bfc574a39a568b57598648d175bc874a
Backo <ckodan@gmail.com>
2020-11-01 19:34:09 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Datatable from "./datatable"
+import { Users } from "./data.js";
+import "./main.css";
 
-function App() {
+export default function App(props) {
+  const [data, setData] = useState([]);
+  const[q, setQ] = useState("");
+
+  useEffect(() => {
+    setData(Users);
+  }, []);
+
+  function search(rows) {
+    const columns = rows[0] && Object.keys(rows[0]);
+
+    return rows.filter(
+      (row) => 
+        columns.some(
+          (column) => row[column].toString().toLowerCase().indexOf(q.toLocaleLowerCase()) > -1)
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <label>Filter: </label> 
+        <input type="text" value={q} onChange={(e) => setQ(e.target.value)}/>
+      </div>      
+      <Datatable data={search(data)} />
     </div>
   );
 }
-
-export default App;
